cfda4579024d3f8ec2be809c635469122728f1c3
gonovahamza <you@example.com>
2020-10-21 23:10:34 +0500

    updated my practice app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-     hamza
+     hamza is here 
+
+     I have updated this app and checking now my new  deployment works fine or not
     </div>
   );
 }
